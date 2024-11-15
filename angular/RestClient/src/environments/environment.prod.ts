a8244eb567add0477606e6e648e2b14651256858
migudel <miguel.moras@estudiantes.uva.es>
2024-11-15 18:30:14 +0100

    Ficheros docker and envs

--- angular/RestClient/src/environments/environment.prod.ts
+++ angular/RestClient/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   authAPI: 'http://auth-api:8080',
-  userAPI: 'http://user-api:8080',
+  userAPI: 'http://users-api:8080',
   hotelAPI: 'http://hotels-api:8080',
   bookingAPI: 'http://bookings-api:8080',
 };
