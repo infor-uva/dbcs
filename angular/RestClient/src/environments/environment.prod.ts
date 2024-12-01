bc54b3dc35a5361f000c5b0e4becc5c5dac2b01a
migudel <miguel.moras@estudiantes.uva.es>
2024-12-01 18:12:16 +0100

    Cambio a entorno de desarrollo angular

--- angular/RestClient/src/environments/environment.prod.ts
+++ angular/RestClient/src/environments/environment.prod.ts
@@ -1,8 +1,17 @@
-const monolithUrl = 'http://rooms-booking-api:8080';
+// const monolithUrl = 'http://rooms-booking-api:8080';
+
+// export const environment = {
+//   production: true,
+//   authAPI: 'http://auth-api:8101',
+//   userAPI: `http://${monolithUrl}/users`,
+//   hotelAPI: `http://${monolithUrl}/hotels`,
+//   bookingAPI: `http://${monolithUrl}/bookings`,
+// };
+const monolithUrl = 'localhost:8080';
 
 export const environment = {
-  production: true,
-  authAPI: 'http://auth-api:8101',
+  production: false,
+  authAPI: 'http://localhost:8101',
   userAPI: `http://${monolithUrl}/users`,
   hotelAPI: `http://${monolithUrl}/hotels`,
   bookingAPI: `http://${monolithUrl}/bookings`,
