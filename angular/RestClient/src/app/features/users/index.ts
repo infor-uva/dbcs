c19885b0eaeb754724325c5fd827475d91ee159f
Hugo <hugo.cubino@estudiantes.uva.es>
2024-12-27 14:41:37 +0100

    Restaurados mocks

--- angular/RestClient/src/app/features/users/index.ts
+++ angular/RestClient/src/app/features/users/index.ts
@@ -2,4 +2,4 @@ export * from './users.routes';
 export * from './main-page/main-page.component';
 export * from './user-form/user-form.component';
 export * from './types/User.interface';
-export * from './types/UserFormData';
+export * from '@features/users/types/UserFormData';
