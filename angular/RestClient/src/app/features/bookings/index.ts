58d5b59d3cf462741e60ec8c0f55975520104bfd
migudel <miguel.moras@estudiantes.uva.es>
2024-12-29 22:32:16 +0100

    README

--- angular/RestClient/src/app/features/bookings/index.ts
+++ angular/RestClient/src/app/features/bookings/index.ts
@@ -1,5 +1,4 @@
 export * from './bookings.routes';
 export * from './booking/booking.component';
-export * from './[DEL] booking-list/booking-list.component';
 export * from './user-booking-list/user-booking-list.component';
-export type { Booking }from '@features/bookings/types/Booking';
+export type { Booking } from '@features/bookings/types/Booking';
